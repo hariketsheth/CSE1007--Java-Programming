5d19cb22d98e27367b1445c5424352fa4500dd74
Hariket Sukeshkumar Sheth <hariket.sukeshkumar2020@gmail.com>
2022-01-11 10:56:25 +0530

    Codes Updated

---
+++ Lab1 Programs/Lab1_Average.java
@@ -0,0 +1,25 @@
+package lab1;
+
+import java.util.Scanner;
+
+public class Lab1_Average
+{
+    public static void main(String[] args) 
+    {
+        int n, sum = 0;
+        float average;
+        Scanner s = new Scanner(System.in);
+        System.out.print("Enter no. of elements you want in array:");
+        n = s.nextInt();
+        int a[] = new int[n];
+        System.out.println("Enter all the elements:");
+        for(int i = 0; i < n ; i++)
+        {
+            a[i] = s.nextInt();
+            sum += a[i];
+        }
+        System.out.println("Sum of the Numbers entered:"+sum);
+        average = (float)sum / n;
+        System.out.println("Average of the Numbers entered:"+average);
+    }
+}
