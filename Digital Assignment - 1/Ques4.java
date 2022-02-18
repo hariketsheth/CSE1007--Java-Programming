d3a540c4e744132cb2658e7b018fb11e7c3b1a2c
Hariket Sukeshkumar Sheth <hariket.sukeshkumar2020@gmail.com>
2022-02-18 22:38:24 +0530

    Added `Digital Assignment 1`

--- Digital Assignment - 1/Ques4.java
+++ Digital Assignment - 1/Ques4.java
@@ -1,3 +1,43 @@
-public class Ques4 {
-    
+package da1;
+import java.util.*;
+import java.lang.Math;   
+
+abstract class SubClass {
+    void announcement(int i) {};
+    void registrations(int n){};
+    void participants(int n){};
+}
+
+interface Input {
+    void accept();
+}
+
+class Parent extends SubClass implements Input{
+        public int num;
+        void announcement(int i){
+            System.out.println("This is the Announcement "+i+" for the New Event.");
+        }
+        void registrations(int n){
+            System.out.println("The weightage of Social Media Round: "+(n+10*Math.random()));
+        }
+        void participants(int n){
+            System.out.println("The weightage of scores: "+(n+90*Math.random()));
+            System.out.println("Updated weightage of scores: "+(n+110*Math.random()));
+        }
+        public void accept(){
+            Scanner sc = new Scanner(System.in);
+            System.out.print("Enter the number of announcements for Event: ");
+            num = sc.nextInt();
+        }
+}
+public class DA1 {
+    public static void main(String args[]) {
+        Parent p1 = new Parent();
+        p1.accept();
+        for(int i=0; i<p1.num; i++){
+            p1.announcement(i+1);
+            p1.registrations(p1.num);
+            p1.participants(p1.num);
+        }
+    }
 }
