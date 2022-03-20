e618f0f0a313b21fe3bec817f7a2e4a2574c213e
Hariket Sukeshkumar Sheth <hariket.sukeshkumar2020@gmail.com>
2022-03-20 07:36:45 +0530

    Modified `Codes`

--- Labs/Lab 8 - Collections/Code4.java
+++ Labs/Lab 8 - Collections/Code4.java
@@ -2,46 +2,50 @@ package lab8;
 import java.io.*;
 import java.util.*;
 
-class Stack{
-    int element, position;
-    Scanner sc = new Scanner(System.in);
-    int push(Stack s1[], int top, int n){
-        int element = sc.nextInt();
-        s1[top].element = element;
-        top += 1;
-        
-        return top;
+class Student {
+    int roll_no, marks;
+    String name;
+    
+    void get(){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter Name, Roll No., Marks: ");
+        name = sc.nextLine();
+        roll_no = sc.nextInt();
+        marks = sc.nextInt();
     }
     
-    int pop(Stack s1[], int top, int n){
-        int element = sc.nextInt();
-        for(Int i=10; i>=0; i--){
-            System.out.print("The Student: "+s1[i].element);
+    void display(Stack<Student> ll, int n){
+        System.out.println("The Student Details are:");
+        for(int i=0; i<n; i++)
+            System.out.println("Name: "+ll.get(i).name+"\tRoll No.: "+ll.get(i).roll_no+"\tMarks: "+ll.get(i).marks);
     }
-    return pop;
-}
 }
 public class Lab8{
     public static void main(String[] args){
-    int element, position;
-    Scanner sc = new Scanner(System.in);
-    int push(Stack s1[], int top, int n){
-        int element = sc.nextInt();
-        s1[top].element = element;
-        top += 1;
+        int n;
+        System.out.print("Enter the number of Students: ");
+        Scanner sc = new Scanner(System.in);
+        n = sc.nextInt();
+        Stack<Student> arr = new Stack<Student>();
+        Student s1[] = new Student[n];
+        for(int i=0; i<n; i++){
+            s1[i] = new Student();
+            s1[i].get();
+            arr.push(s1[i]);
+        }
         
-        return top;
+        System.out.println("\nThe contents of Stack are: ");
+        s1[0].display(arr,n);
+        arr.pop();
+        n-=1;
+         System.out.println("\nThe contents of Stack are: ");
+        s1[0].display(arr,n);
+        System.out.println("Enter a new element: ");
+        Student add = new Student();
+        add.get();
+        arr.push(add);
+        n += 1;
+        System.out.println("\nThe contents of Stack are: ");
+        s1[0].display(arr,n);
     }
-    
-    int pop(Stack s1[], int top, int n){
-        int element = sc.nextInt();
-        for(Int i=10; i>=0; i--){
-            System.out.print("The Student: "+s1[i].element);
-    }
-    return pop;
-}
-    }
-    int number = Scanner sc.next.charAt(i);
-    for(int i=0; i<n; i++)
-        Stack s1
 }
